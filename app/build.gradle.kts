d48e3dc4574b37e6da73de42895bf81c55116f3b
ozzylatorcl-labs <ozzylatorcl@gmail.com>
2026-09-04 00:11:21 -0400

    Cosmora TV V1.5 version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "cl.ozzylatorlabs.cosmoratv"
         minSdk = 23
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
     }
 
     buildFeatures { compose = true }
